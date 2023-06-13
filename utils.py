13b8254032362b6d88a12c21e311a5ec7f802285
Hyunwook Jo <acerghjk@gmail.com>
2023-06-13 18:10:05 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 # GLUE STS 내 훈련, 검증 데이터 예제 변환
 from sentence_transformers.readers import InputExample
 import os
+import pandas as pd
+import sqlite3
 
 def Preprocess(datasets):
   train_samples = []
@@ -27,8 +29,27 @@ def Preprocess(datasets):
 
 
 
+
+# SQLite3 연결
+def get_db():
+  conn = sqlite3.connect('url_check_6_6.db')
+
+  # 쿼리 실행 및 데이터프레임 생성
+  query = 'SELECT * FROM tweet;'
+  ex = pd.read_sql_query(query, conn)
+  raw = ex[['companyName','tweetDate', 'rawContent']]
+
+  raw_all = raw.rawContent.values.tolist()
+  raw_all = raw_all[:100]
+  print('db 얻기 ',type(raw_all))
+  return raw_all
+
+
+
 def setup(args):
   output_path = args.output_path
   output_path = output_path.split('/')[-1]
   os.makedirs(output_path, exist_ok=True)
+  
+
   
