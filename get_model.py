a15d4bf5a09bd874ab8acab367d4765e9b690600
Hyunwook Jo <acerghjk@gmail.com>
2023-06-13 17:05:19 +0900

    Create get_model.py

---
+++ get_model.py
@@ -0,0 +1,23 @@
+from sentence_transformers import SentenceTransformer, LoggingHandler, losses, models, util
+def Get_model(args):
+
+
+  embedding_model = models.Transformer(model_name_or_path=args.model_name,
+                                     max_seq_length=args.max_seq_length,
+                                     do_lower_case=args.do_lower
+                                     )
+
+
+  pooler = models.Pooling(
+    embedding_model.get_word_embedding_dimension(),
+    pooling_mode_mean_tokens=True,
+    pooling_mode_cls_token=args.cls_token,
+    pooling_mode_max_tokens=True,
+  )
+
+
+  model = SentenceTransformer(modules=[embedding_model, pooler])
+
+
+  return model
+
