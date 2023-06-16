e2f2f9f2080affa1be81262d9a3539f80203625f
Hyunwook Jo <acerghjk@gmail.com>
2023-06-16 16:57:39 +0900

    Update get_topic.py

--- get_topic.py
+++ get_topic.py
@@ -2,12 +2,10 @@ from bertopic import BERTopic
 import os
 
 def Get_topic(args):
-  output_path = 'out'
-  folder_path = f"./{output_path}"  # 'out' 폴더의 경로
-  file_list = os.listdir(folder_path)
 
-
-  load_model = BERTopic.load(f"./{output_path}/raw_all_model")
+  model_name = args.model_name.replace("/", "-")
+  output_path = args.output_path
+  load_model = BERTopic.load(f"{output_path}/raw_all_model")
   barchart = load_model.visualize_barchart()
 
-  return barchart.write_image(f"./barchart.png")
+  return barchart.write_image(f"{output_path}/{model_name}_barchart.png")
