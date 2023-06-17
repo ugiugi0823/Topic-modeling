09d2746ad4f29ffe9d0a154dfce96573654a959b
Hyunwook Jo <acerghjk@gmail.com>
2023-06-17 13:02:11 +0900

    Update get_topic.py

--- get_topic.py
+++ get_topic.py
@@ -5,7 +5,13 @@ def Get_topic(args):
 
   model_name = args.model_name.replace("/", "-")
   output_path = args.output_path
+  print('BERTopic Topic Modeling')
   load_model = BERTopic.load(f"{output_path}/raw_all_model")
+  print('BERTopic, Making Barchart')
   barchart = load_model.visualize_barchart()
-
-  return barchart.write_image(f"{output_path}/{model_name}_barchart.png")
+  print('Barchart 를 저장하고 있습니다.')
+  if args.drive:
+    barchart.write_image(f"/content/drive/Mydrive/inisw08/bertopic/barchart/{model_name}_barchart.png")
+  else:
+    barchart.write_image(f"/content/inisw08/bertopic/barchart/{model_name}_barchart.png")
+  return print('EveryThing is END')
