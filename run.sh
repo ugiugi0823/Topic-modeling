c0e85458ae8f20354f466b539d805415863008a7
Hyunwook Jo <acerghjk@gmail.com>
2023-06-19 00:17:55 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,35 @@
-pip install bertopic
-pip install sentence-transformers datasets
-pip install -U kaleido
+
+pip install transformers
+pip install wandb
+# 허깅페이스 로그인 토큰
+# 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
 gdown '1wVh8tP0XcOuabv9EUa6hnXiykqjTqQ1t&confirm=t'
-unzip database_csv.zip -d /content/topic-modeling/data
+unzip database_csv.zip -d /content/tweet-sa-robert/data
+
+huggingface-cli login --token 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
+# inisw (중요)
+# 2be184e31a96c722bfebdfe35f726042eb8e526c
+# 현욱
+# 122f007f67ba33fd04a03ee9b81489dfb42264a6
+wandb login --relogin '122f007f67ba33fd04a03ee9b81489dfb42264a6'
+
+
 
 
 
 python main.py \
-  --output_path "/content/drive/MyDrive/inisw08/bertopic" \
-  --model_name "ugiugi/inisw08-RoBERT-mlm-adamw_torch_bs16" \
-  --max_seq_length 512 \
-  --batch_size 64 \
-  --n_epoch 1 \
-  --use_amp True \
-  --cls_token True \
-  --do_lower True \
-  --db_name "preproc_6_2.csv" \
-  --drive True
+    --drive True \
+    --data 'result_all' \
+    --batch_size 256 \
+    --optimizer 'sgd' \
+    --learning_rate 5.999e-5 \
+    --epochs 30 \
+    --run_name 'inisw08-twitter-sentiment-analysis-roberta_ep30_bs256_all' \
+    --project_name 'inisw08-twitter-sentiment-analysis-roberta' \
+    --entity_name 'inisw08' \
+    --model_fold_name 'inisw08-twitter-sentiment-analysis-roberta' \
+    --test False \
+    --test_data 'test_1000' \
+    --test_model_name 'ugiugi/inisw08-twitter-sentiment-analysis-roberta-ep30-bs256-all' \
+    --project_test_name 'which-SA-model-is-test'
+    
