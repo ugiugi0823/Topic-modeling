904b1b4169c7cbab4b1c47e35228e8470d739a22
Hyunwook Jo <acerghjk@gmail.com>
2023-06-18 15:51:31 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+pip install bertopic
+pip install sentence-transformers datasets
+pip install -U kaleido
+gdown '1wVh8tP0XcOuabv9EUa6hnXiykqjTqQ1t&confirm=t'
+unzip database_csv.zip -d /content/topic-modeling/data
